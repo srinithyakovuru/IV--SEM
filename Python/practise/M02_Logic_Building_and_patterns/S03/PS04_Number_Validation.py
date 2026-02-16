5cc66968e5f905c017621918fd12f4a9e0bc6887
srinithyakovuru <kovurusrinithyareddy@gmail.com>
2026-02-16 14:42:38 +0530

    mes

--- Python/practise/M02_Logic_Building_and_patterns/S03/PS04_Number_Validation.py
+++ Python/practise/M02_Logic_Building_and_patterns/S03/PS04_Number_Validation.py
@@ -6,13 +6,35 @@ Armstrong Number: A number is called an Armstrong number if it is equal to the s
 input:153
 output:Armstrong number
 
-input:24
+input:24  
 output:Not an Armstrong number
 '''
-n=int(input())
+'''n=int(input())
 count=len(str(n))
 s=0
-for i in str(n):
-    s=s+int(i)**count
+for digit in str(n):
+    s=s+int(digit)**count   
 print("Armstrong number" if s==n else "Not an Armstrong number")
+
+strong number:
+input:123
+output:Not a strong number
+Explanation: 1! + 2! + 3! = 9 which is not equal to 123'''
+
+def factorial(n):
+    if n<0:
+        return "no factorial for -ve"
+    elif n==0 or n==1:
+        return 1
+    else:
+        fact=1
+        for i in range(1,n+1):
+            fact=fact*i
+        return fact
+n=int(input())
+s=0
+for digit in str(n):
+    s+=factorial(int(digit))
+print("strong number" if s==n else "Not a strong number")
+
 
---
+++ Python/practise/M02_Logic_Building_and_patterns/S03/PS05 Basic_Patterns.py
@@ -0,0 +1,50 @@
+
+'''input:4
+output:
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print("*",end=" ")
+    print()
+'''
+n=4
+*
+* *
+* * *
+* * * *
+'''
+'''n=int(input())
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end=" ")
+    print()'''
+
+'''
+n=4
+A
+A B
+A B C
+A B C D
+'''
+'''
+n=4
+1
+1 2
+4 5 6
+7 8 9 10
+'''
+'''
+n=4
+****
+*  *
+*  *
+****
+'''
+'''
+stair case in hackerrank    
+'''
